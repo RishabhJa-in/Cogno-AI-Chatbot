c1802221410da15af17709b85f2e33007829823c
Rishabh Jain <98253273+RishabhJa-in@users.noreply.github.com>
2022-03-15 15:10:43 +0530

    version --1.0.0

---
+++ script.js
@@ -0,0 +1,14 @@
+let box=document.getElementById("box1")
+    box.style.display="none";
+    function fun()
+    {
+        if(box.style.display=="none")
+        {
+box.style.display="block";
+        }
+        else
+        {
+box.style.display="none";
+        }
+        
+    }
